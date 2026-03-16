ba8f87390eb1715e46eddf6262ab264cfd3bba2b
Liran Martfel <liran.martfel@gmail.com>
2026-03-16 13:18:06 +0200

    תיקון שיעורי בית

--- Homework.py
+++ Homework.py
@@ -11,26 +11,19 @@ amount_ = get_lucky_numbers()
 print(amount_)
 
 def input_until_lucky(amount: tuple) -> int:
-    tries = 1
-    try:
-        lucky_number = int(input('enter A number: '))
-        print(lucky_number)
-    except ValueError:
-        print('need positive integer, not str, try again')
+    tries = 0
+    while True:
+        try:
+            lucky_number = int(input('enter A number: '))
+            tries += 1
+            if lucky_number < 1:
+                print('try positive numbers')
+                continue
+            if lucky_number in amount:
+                print(f'luckey_number! {lucky_number}, you tried {tries} times')
+                return lucky_number
 
-    lucky_number = int(input('enter a number: '))
-    if lucky_number < 1:
-        print('try positive numbers')
-        lucky_number = int(input('enter a number: '))
-        tries += 1
-
-    while lucky_number not in amount:
-        lucky_number = int(input('enter a number: '))
-        tries += 1
-
-    else:
-        print(f'lucky_number! {lucky_number}, you tried {tries} times')
-        return lucky_number
-
+        except ValueError:
+            print('need positive integer, not str, try again')
 
 print(input_until_lucky(amount_))
